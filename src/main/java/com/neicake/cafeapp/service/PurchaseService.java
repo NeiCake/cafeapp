0189f1dacf3cdc0612d0d9212c21944049bcdf30
Neiyla Anda Medveşan <neiyla.anda.medvesan@accenture.com>
2017-10-17 09:26:26 +0300

    accepting purchases with no customer/coupon selected

--- src/main/java/com/neicake/cafeapp/service/PurchaseService.java
+++ src/main/java/com/neicake/cafeapp/service/PurchaseService.java
@@ -16,72 +16,72 @@ import java.util.*;
 @Service
 public class PurchaseService implements IPurchaseService {
 
-    @Autowired
-    private PurchaseRepository purchaseDao;
+	@Autowired
+	private PurchaseRepository purchaseDao;
 
-    @Autowired
-    private ProductRepository productDao;
+	@Autowired
+	private ProductRepository productDao;
 
-    @Autowired
-    private ProductDiscountRepository productDiscountDao;
+	@Autowired
+	private ProductDiscountRepository productDiscountDao;
 
-    @Autowired
-    private CouponRepository couponDao;
+	@Autowired
+	private CouponRepository couponDao;
 
-    @Autowired
-    private CustomerRepository customerDao;
+	@Autowired
+	private CustomerRepository customerDao;
 
-    @Override
-    public boolean validatePurchase(Purchase purchase) {
-        if(purchase.getAmount()==0) return false;
-        if(purchase.getAmount()<=purchase.getProduct().getStock()){
-            return true;
-        }
-	    System.out.println("not enough stock");
-	    return false;
-    }
+	@Override
+	public boolean validatePurchase(Purchase purchase) {
+		if (purchase.getAmount() == 0) return false;
+		if (purchase.getAmount() <= purchase.getProduct().getStock()) {
+			return true;
+		}
+		System.out.println("not enough stock");
+		return false;
+	}
 
-    @Override
-    public boolean performPurchase(Purchase purchase) {
-        if(validatePurchase(purchase)) {
-            int remainingStock=purchase.getProduct().getStock()-purchase.getAmount();
-            purchase.getProduct().setStock(remainingStock);
-            productDao.save(purchase.getProduct());
-            purchaseDao.save(purchase);
-            return true;
+	@Override
+	public boolean performPurchase(Purchase purchase) {
+		if (validatePurchase(purchase)) {
+			int remainingStock = purchase.getProduct().getStock() - purchase.getAmount();
+			purchase.getProduct().setStock(remainingStock);
+			productDao.save(purchase.getProduct());
+			purchaseDao.save(purchase);
+			return true;
 
-        }
-        return false;
-    }
+		}
+		return false;
+	}
 
-    @Override
-    public List<Purchase> listAllPurchases() {
-        List<Purchase> list= purchaseDao.findAll();
+	@Override
+	public List<Purchase> listAllPurchases() {
+		List<Purchase> list = purchaseDao.findAll();
 //        for(Purchase p:list){
 //	        p=completeData(p);
 //        }
-        return list;
-    }
+		return list;
+	}
 
-    @Override
-    public List<Purchase> listAllPurchasesForPeriod(Date start, Date end) {
+	@Override
+	public List<Purchase> listAllPurchasesForPeriod(Date start, Date end) {
 
-	    List<Purchase> list= purchaseDao.findAllByDateBetween(start,end);
+		List<Purchase> list = purchaseDao.findAllByDateBetween(start, end);
 //	    for(Purchase p:list){
 //		    p=completeData(p);
 //	    }
-	    return list;
-    }
+		return list;
+	}
 
-    @Override
-    public List<Purchase> listAllPurchasesForLastDays(int days) {
-        GregorianCalendar cal = new GregorianCalendar();
-        cal.setTime(new Date());
-        cal.add(Calendar.DATE, -days);
-        Date start=new Date();
-        Date end= cal.getTime();
-        return listAllPurchasesForPeriod(start, end);
-    }
+	@Override
+	public List<Purchase> listAllPurchasesForLastDays(int days) {
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.setTime(new Date());
+		cal.add(Calendar.DATE, -days);
+		Date start = new Date();
+		Date end = cal.getTime();
+		return listAllPurchasesForPeriod(start, end);
+	}
 
   /*  @Override
     public Purchase completeData(Purchase purchase) {
@@ -94,32 +94,41 @@ public class PurchaseService implements IPurchaseService {
 
 	@Override
 	public Purchase buildPurchaseFromString(String purchaseString) {
-		String[] array=purchaseString.split("\\+");
-		System.out.println("array length is: "+array.length);
+		String[] array = purchaseString.split("\\+");
+		System.out.println("array length is: " + array.length);
 		System.out.println(array);
-		if(array.length!=6){
+		if (array.length != 6) {
 			System.out.println("MISSING PARAMETER");
 			throw new InvalidPurchaseException("MISSING A PARAMETER");
 		}
-		Purchase purchase=new Purchase();
-		if(!array[0].equals("0")){
-			Product product=productDao.getOne(Long.valueOf(array[0]));
+		Purchase purchase = new Purchase();
+		if (!array[0].equals("0")) {
+			Product product = productDao.getOne(Long.valueOf(array[0]));
 			purchase.setProduct(product);
 		}
-		if(!array[1].equals("0")){
-			ProductDiscount productDiscount=productDiscountDao.findOne(Long.valueOf(array[1]));
+		if (!array[1].equals("0")) {
+			ProductDiscount productDiscount = productDiscountDao.findOne(Long.valueOf(array[1]));
 			purchase.setProductDiscount(productDiscount);
 		}
-		if(!array[2].equals("0")){
-			Customer customer=customerDao.findOne(Long.valueOf(array[2]));
-			purchase.setCustomer(customer);
+		if (!array[2].equals("0")) {
+			try{
+				Customer customer = customerDao.findOne(Long.valueOf(array[2]));
+				purchase.setCustomer(customer);
+			}catch (NumberFormatException e){
+				//leaving customer null if there is no customer
+			}
 		}
-		if(!array[3].equals("0")){
-			Coupon coupon=couponDao.findOne(Long.valueOf(array[3]));
-			purchase.setCoupon(coupon);
+		if (!array[3].equals("0")) {
+
+			try{
+				Coupon coupon = couponDao.findOne(Long.valueOf(array[3]));
+				purchase.setCoupon(coupon);
+			}catch (NumberFormatException e){
+				//leaving coupon null if there is no coupon
+			}
 		}
 
-		if(array[4].equals("")) {
+		if (array[4].equals("")) {
 			System.out.println("no amount was input");
 			throw new InvalidPurchaseException("EMPTY STRING AMOUNT");
 		}
